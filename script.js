56de311ae350bbd779074f2f6d3f1d9960233e42
agamenonlu <luisbarrio79a@gmail.com>
2026-01-19 22:30:42 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ La teva tasca és:
 
     try {
         const response = await fetch(
-            "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-pro:generateContent?key=AIzaSyBp6ZaopjxSXnslYxv_qsAL07T-0QlrO0U",
+            "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyBp6ZaopjxSXnslYxv_qsAL07T-0QlrO0U",
             {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
